0c98364bb9112dec3dc5de9b971365f89d3da2ca
v1shn3vsk7 <v1shn3vsk7@inbox.ru>
2022-04-06 13:12:46 +0300

    Header is done

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Penkoff_ASP.NET_Core_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

7d8a85cbd75d35a4d11317d872360f3716f9a03f
v1shn3vsk7 <v1shn3vsk7@inbox.ru>
2022-04-19 20:21:40 +0300

    sign up page is done

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Penkoff_ASP.NET_Core_.Models;
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+
+        }
+    }

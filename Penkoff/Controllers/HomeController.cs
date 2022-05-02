cc5310618c42619bf87d8a79a4d1a83e2e46f32d
v1shn3vsk7 <v1shn3vsk7@inbox.ru>
2022-05-02 19:01:50 +0300

    Updated project directory x2

---
+++ Penkoff/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Penkoff_ASP.NET_Core_.Models;
+using System.Diagnostics;
+
+namespace Penkoff_ASP.NET_Core_.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Authorization()
+        {
+            return View();
+        }
+
+        public IActionResult SignUpPage()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

3312e675bad1c0739603c62235325a4378f490b6
v1shn3vsk7 <v1shn3vsk7@inbox.ru>
2022-05-15 23:19:54 +0300

    fixed Penkoff.Logic folder

---
+++ Penkoff.Logic/Users/IUserManager.cs
@@ -0,0 +1,9 @@
+﻿namespace Penkoff.Logic.Users
+{
+    public interface IUserManager
+    {
+        Task<IList<User>> GetAll();
+        Task Create(string Login, string Password, string FirstName, string LastName);
+        Task Delete(int Id);
+    }
+}
---
+++ Penkoff.Logic/Users/UserManager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Penkoff.Storage;
+
+namespace Penkoff.Logic.Users;
+
+public class UserManager : IUserManager
+{
+    private readonly UsersContext _context;
+
+    public UserManager(UsersContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IList<User>> GetAll() => await _context.Users.ToListAsync();
+
+    public async Task Create(string login, string password, string firstName, string lastName)
+    {
+        var user = new User { Login = login, Password = password, FirstName = firstName, LastName = lastName };
+
+        _context.Users.Add(user);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Delete(int id)
+    {
+        //var user = _context.Users.FirstOrDefault(u => u.Id == id);
+
+        var user = _context.Users.Find(id);
+
+        if (user != null)
+        {
+            _context.Users.Remove(user);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}

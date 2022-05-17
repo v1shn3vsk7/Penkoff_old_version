19c2064eec850617525c8a494fa12dc758786cf9
v1shn3vsk7 <v1shn3vsk7@inbox.ru>
2022-05-17 21:32:32 +0300

    fixed button position in Login and Sign up

--- Penkoff.Storage/Entities/User.cs
+++ Penkoff.Storage/Entities/User.cs
@@ -4,9 +4,13 @@ public class User
 {
     [Key]
     public int Id { get; set; }
+
     public string Login { get; set; }
+
     public string Password { get; set; }
+
     public string FirstName { get; set; }
+
     public string LastName { get; set; }
 
    // [Required]

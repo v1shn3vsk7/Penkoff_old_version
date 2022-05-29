7fc9c79887010cf42225772fb389ed6f1bc963d3
v1shn3vsk7 <v1shn3vsk7@inbox.ru>
2022-05-29 10:52:44 +0300

    added operation entity, updated DB

---
+++ Penkoff.Storage/Entities/Operation.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Penkoff.Storage.Entities;
+
+public class Operation
+{
+    [Key]
+    public int UserId { get; set; }
+
+    public string Currency { get; set; }
+
+    public string Type { get; set; }
+
+    public uint Amount { get; set; }
+
+    [ForeignKey(nameof(UserId))]
+    public User User { get; set; }
+}
+

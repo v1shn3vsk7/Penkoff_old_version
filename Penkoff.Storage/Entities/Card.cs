dfa9922058ffd8d214aa2be5624b745a557e1a76
v1shn3vsk7 <v1shn3vsk7@inbox.ru>
2022-05-30 12:29:55 +0300

    added Card Entity

---
+++ Penkoff.Storage/Entities/Card.cs
@@ -0,0 +1,17 @@
+﻿namespace Penkoff.Storage.Entities;
+
+public class Card
+{
+    [Key]
+    public Int64 Pan { get; set; } //card number
+
+    public DateTime ExpirationDate { get; set; }
+
+    public uint CVV { get; set; }
+
+    public int UserId { get; set; }
+
+    [ForeignKey(nameof(UserId))]
+    public User User { get; set; }
+}
+

9485090c708a0ab59c5317fe57e72b13ef27d11e
v1shn3vsk7 <v1shn3vsk7@inbox.ru>
2022-05-31 15:26:05 +0300

    added 'Stocks' Page

--- Penkoff/Controllers/HomeController.cs
+++ Penkoff/Controllers/HomeController.cs
@@ -24,6 +24,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Stocks()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
